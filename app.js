83054f41cd16537a6eecf2f6d96d82599acbd344
Axmouth <aggronfg@yahoo.com>
2019-11-07 22:37:09 +0200

    It's been a while since I changed this so no clue, updating due to security vulnerrbilities. Seems I added a bunch of events, moved from sequelize to enmap. And cleaned up some files, specially things that shouldn't be there tbh.

--- app.js
+++ app.js
@@ -1,161 +1,209 @@
-secret = "NDg1OTY3NzU2MDY4NjUxMDA4.DoeVXQ.W7rwO5eoO0WOQ52tmfgAR8c1Upc";
-inv_link = "https://discordapp.com/api/oauth2/authorize?client_id=485967756068651008&permissions=363584&scope=bot";
-log_chan_id = "393862821491900437";
-
-const Discord = require('discord.js');
-const Sequelize = require('sequelize');
+const config = require("./config/config.json");
+const staticData = require("./staticData.js");
+const Discord = require("discord.js");
+const Enmap = require("enmap");
 
 const client = new Discord.Client();
 
-var log_chan;
-
-const sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    operatorsAliases: false,
-
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-
-    // SQLite only
-    storage: './DB/test.database.sqlite'
+client.settings = new Enmap({
+  name: "settings"
 });
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+const eventsList = staticData.eventsList;
+const defaultSettings = staticData.defaultSettings;
 
-function sendMessage(content, guild) {
-
+async function sendMessage(content, guild) {
+  var guildConf = client.settings.ensure(guild.id, defaultSettings);
+  if (client.settings.has(guild.id, "loggingChannel")) {
+    var log_channel_id = client.settings.get(guild.id, "loggingChannel");
+    if (log_channel_id !== -1) {
+      var log_channel = client.channels.get(log_channel_id);
+      return await log_channel.send(content);
+    }
+  }
 }
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-
-    log_chan = client.channels.get("393862821491900437");
-    // console.log(log_chan);
+client.on("guildDelete", guild => {
+  // Removing an element uses `delete(key)`
+  client.settings.delete(guild.id);
 });
 
-/*
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on("ready", () => {
+  client.user
+    .setActivity("your every move.", {
+      type: "WATCHING"
+    })
+    .then(presence =>
+      console.log(
+        `Activity set to ${presence.game ? presence.game.name : "none"}`
+      )
+    )
+    .catch(console.error);
+  console.log(`Logged in as ${client.user.tag}!`);
+  // console.log(log_chan);
+});
+
+client.on("message", async msg => {
+  if (msg.isMentioned(client.user)) {
+    // First, ensure the settings exist
+    client.settings.ensure(msg.guild.id, defaultSettings);
+    // msg.reply('message here');
+    if (msg.content.toLowerCase().includes("setlogchannel")) {
+      client.settings.set(msg.guild.id, msg.channel.id, "loggingChannel");
+    } else if (msg.content.toLowerCase().includes("dashboard")) {
+      createDashboard(msg.guild);
+    } else if (msg.content.toLowerCase().includes("test")) {
+      console.log((await sendMessage("test", msg.guild)).id);
+    }
   }
 });
-*/
 
-client.on('messageDelete', msg => {
-    var replyMsg = "";
-    replyMsg += `Message deleted by **${msg.author.tag}** (ID: **${msg.author.id}**)\n`;
+client.on("messageDelete", msg => {
+  var replyMsg = "";
+  replyMsg += `Message deleted by **${msg.author.tag}** (ID: **${
+    msg.author.id
+  }**)\n`;
+  var attachments = msg.attachments.array();
+  if (attachments.length > 0) {
+    replyMsg += "► Attachments:\n";
+    var i = 0;
+    for (i = 0; i < attachments.length; i++) {
+      replyMsg += attachments[i].proxyURL + "\n";
+    }
+  }
+  if (msg.content) {
+    replyMsg += `► Content:\n\`\`\`${msg.content}\`\`\``;
+  }
+  if (replyMsg) {
+    sendMessage(replyMsg, msg.guild);
+  }
+});
+
+client.on("messageDeleteBulk", messages => {
+  var replyMsg = "";
+  const messagesArray = messages.array();
+  replyMsg += `${messagesArray.length} Messages deleted in bulk\n`;
+  var messageCounter;
+  for (
+    messageCounter = 0; messageCounter < messagesArray.length; messageCounter++
+  ) {
+    msg = messagesArray[messageCounter];
+    replyMsg += "► Author: **${msg.author.tag}** (ID: **${msg.author.id}**)\n";
     var attachments = msg.attachments.array();
     if (attachments.length > 0) {
-        replyMsg += "► Attachments:\n";
-        var i = 0;
-        for (i = 0; i < attachments.length; i++) {
-            replyMsg += attachments[i].proxyURL + "\n";
-        }
-
+      replyMsg += "► Attachments:\n";
+      var i = 0;
+      for (i = 0; i < attachments.length; i++) {
+        replyMsg += attachments[i].proxyURL + "\n";
+      }
     }
     if (msg.content) {
-        replyMsg += `► Content:\n\`\`\`${msg.content}\`\`\``;
+      replyMsg += `► Content:\n\`\`\`${msg.content}\`\`\``;
     }
-    if (replyMsg) {
-        log_chan.send(replyMsg);
+    replyMsg += "\n\n";
+  }
+  if (replyMsg) {
+    sendMessage(replyMsg, msg.guild);
+  }
+});
+
+client.on("messageUpdate", (oldMsg, newMsg) => {
+  var replyMsg = "";
+  replyMsg += `Message edited by **${oldMsg.author.tag}** (ID: **${
+    oldMsg.author.id
+  }**)\n`;
+  var attachments = oldMsg.attachments.array();
+  if (attachments.length > 0) {
+    replyMsg += "► Attachments:\n";
+    var i = 0;
+    for (i = 0; i < attachments.length; i++) {
+      replyMsg += attachments[i].proxyURL + "\n";
     }
+  }
+  replyMsg += `► Old Content:\n\`\`\` \n${oldMsg.content}\n \`\`\`\n`;
+  replyMsg += `► New Content:\n\`\`\` \n${newMsg.content}\n \`\`\`\n`;
+
+  if (replyMsg) {
+    sendMessage(replyMsg, oldMsg.guild);
+  }
 });
 
-client.on('messageDeleteBulk', messages => {
-    var replyMsg = "";
-    const messagesArray = messages.array();
-    replyMsg += `${messagesArray.length} Messages deleted in bulk\n`;
-    var messageCounter;
-    for (messageCounter = 0; messageCounter < messagesArray.length; messageCounter++) {
-        msg = messagesArray[messageCounter];
-        replyMsg += "► Author: **${msg.author.tag}** (ID: **${msg.author.id}**)\n";
-        var attachments = msg.attachments.array();
-        if (attachments.length > 0) {
-            replyMsg += "► Attachments:\n";
-            var i = 0;
-            for (i = 0; i < attachments.length; i++) {
-                replyMsg += attachments[i].proxyURL + "\n";
-            }
+client.on("roleCreate", role => {
+  sendMessage(`Role **${role.name}** was created.\n`, role.guild);
+});
 
-        }
-        if (msg.content) {
-            replyMsg += `► Content:\n\`\`\`${msg.content}\`\`\``;
-        }
-        replyMsg += "\n\n";
+client.on("roleDelete", role => {
+  sendMessage(`Role **${role.name}** was deleted.\n`, role.guild);
+});
+
+client.on("roleUpdate", role => {
+  sendMessage(`Role **${role.name}** was updated.\n`, role.guild);
+});
+
+client.on("guildBanAdd", (guild, user) => {
+  sendMessage(
+    `User **${user.tag}** (ID: **${user.id}**) has been banned from **${
+      guild.name
+    }**.\n`,
+    guild
+  );
+});
+
+client.on("guildBanRemove", (guild, user) => {
+  sendMessage(
+    `User **${user.tag}** (ID: **${
+      user.id
+    }**) has had their banned removed from **${guild.name}**.\n`,
+    guild
+  );
+});
+
+client.on("emojiCreate", emoji => {
+  sendMessage(`Emoji **:${emoji.name}:** was created.\n`, emoji.guild);
+  const attachment = new Discord.Attachment(emoji.url);
+  sendMessage(attachment, emoji.guild);
+});
+
+client.on("emojiDelete", emoji => {
+  sendMessage(`Emoji **:${emoji.name}:** was deleted.\n`, emoji.guild);
+  const attachment = new Discord.Attachment(emoji.url);
+  sendMessage(attachment, emoji.guild);
+});
+
+client.on("emojiUpdate", (oldEmoji, newEmoji) => {
+  sendMessage(`Emoji **:${oldEmoji.name}:** was updated.\n`, emoji.guild);
+  const attachment = new Discord.Attachment(oldEmoji.url);
+  sendMessage(attachment, emoji.guild);
+});
+
+(async function () {
+  await client.settings.defer;
+  console.log(client.settings.size + " keys loaded");
+  // Ready to use!
+  client.login(config.token);
+})();
+
+async function createDashboard(guild) {
+  var i;
+  for (i = 0; i < eventsList.length; i++){
+    if(!eventsList[i].loggable) {
+      continue;
     }
-    if (replyMsg) {
-        log_chan.send(replyMsg);
+    var guildConf = client.settings.ensure(guild.id, defaultSettings);
+    var pendingMessage = eventsList[i].name + eventsList[i].description;
+    var toggled = client.settings.get(guild.id, eventsList[i].name);
+    var toggledReact;
+    if(toggled) {
+      toggledReact = "✅";
+      toggledString = "❌";
     }
-});
-
-client.on('messageUpdate', (oldMsg, newMsg) => {
-    var replyMsg = "";
-    replyMsg += `Message edited by **${oldMsg.author.tag}** (ID: **${oldMsg.author.id}**)\n`;
-    var attachments = oldMsg.attachments.array();
-    if (attachments.length > 0) {
-        replyMsg += "► Attachments:\n";
-        var i = 0;
-        for (i = 0; i < attachments.length; i++) {
-            replyMsg += attachments[i].proxyURL + "\n";
-        }
-
+    else {
+      toggledReact = "❌";
+      toggledString = "✅";
     }
-    replyMsg += `► Old Content:\n\`\`\` \n${oldMsg.content}\n \`\`\`\n`;
-    replyMsg += `► New Content:\n\`\`\` \n${newMsg.content}\n \`\`\`\n`;
-
-    if (replyMsg) {
-        log_chan.send(replyMsg);
-    }
-});
-
-client.on('roleCreate', role => {
-    log_chan.send(`Role **${role.name}** was created.\n`);
-});
-
-client.on('roleDelete', role => {
-    log_chan.send(`Role **${role.name}** was deleted.\n`);
-});
-
-client.on('roleUpdate', role => {
-    log_chan.send(`Role **${role.name}** was updated.\n`);
-});
-
-client.on('guildBanAdd', (guild, user) => {
-    log_chan.send(`User **${user.tag}** (ID: **${user.id}**) has been banned from **${guild.name}**.\n`);
-});
-
-client.on('guildBanRemove', (guild, user) => {
-    log_chan.send(`User **${user.tag}** (ID: **${user.id}**) has had their banned removed from **${guild.name}**.\n`);
-});
-
-client.on('emojiCreate', emoji => {
-    log_chan.send(`Emoji **:${emoji.name}:** was created.\n`);
-    const attachment = new Discord.Attachment(emoji.url);
-    log_chan.send(attachment);
-});
-
-client.on('emojiDelete', emoji => {
-    log_chan.send(`Emoji **:${emoji.name}:** was deleted.\n`);
-    const attachment = new Discord.Attachment(emoji.url);
-    log_chan.send(attachment);
-});
-
-client.on('emojiUpdate', (oldEmoji, newEmoji) => {
-    log_chan.send(`Emoji **:${oldEmoji.name}:** was updated.\n`);
-    const attachment = new Discord.Attachment(oldEmoji.url);
-    log_chan.send(attachment);
-});
-
-client.login(secret);
+    pendingMessage += " " + toggledString;
+    sentMessage = await sendMessage(pendingMessage, guild);
+    await sentMessage.react(toggledReact);
+  }
+  
+}
---
+++ staticData.js
@@ -0,0 +1,268 @@
+const eventsList = [{
+        name: "channelCreate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "channelDelete",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "channelPinsUpdate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "channelUpdate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "clientUserGuildSettingsUpdate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "clientUserSettingsUpdate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "debug",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "disconnect",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "emojiCreate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "emojiDelete",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "emojiUpdate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "error",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "guildBanAdd",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "guildBanRemove",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "guildMemberAdd",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "guildMemberAvailable",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "guildMemberRemove",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "guildMembersChunk",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "guildMemberSpeaking",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "guildMemberUpdate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "guildUnavailable",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "guildUpdate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "message",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "messageDelete",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "messageDeleteBulk",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "messageReactionAdd",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "messageReactionRemove",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "messageReactionRemoveAll",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "messageUpdate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "presenceUpdate",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "rateLimit",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "ready",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "reconnecting",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "resume",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "roleCreate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "roleDelete",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "roleUpdate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "typingStart",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "typingStop",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "userNoteUpdate",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "userUpdate",
+        description: "",
+        loggable: true
+    },
+    {
+        name: "voiceStateUpdate",
+        description: "",
+        loggable: false
+    },
+    {
+        name: "warn",
+        description: "",
+        loggable: false
+    }
+];
+
+const defaultSettings = {
+    channelCreate: true,
+    channelDelete: true,
+    channelPinsUpdate: true,
+    channelUpdate: true,
+    clientUserGuildSettingsUpdate: true,
+    clientUserSettingsUpdate: true,
+    // debug: true,
+    // disconnect: true,
+    emojiCreate: true,
+    emojiDelete: true,
+    emojiUpdate: true,
+    // error: true,
+    guildBanAdd: true,
+    guildBanRemove: true,
+    // guildCreate: true,
+    // guildDelete: true,
+    guildMemberAdd: true,
+    // guildMemberAvailable: true,
+    guildMemberRemove: true,
+    guildMembersChunk: true,
+    // guildMemberSpeaking: true,
+    guildMemberUpdate: true,
+    // guildUnavailable: true,
+    guildUpdate: true,
+    message: true,
+    messageDelete: true,
+    messageDeleteBulk: true,
+    // messageReactionAdd: true,
+    // messageReactionRemove: true,
+    // messageReactionRemoveAll: true,
+    messageUpdate: true,
+    // presenceUpdate: true,
+    // rateLimit: true,
+    // ready: true,
+    // reconnecting: true,
+    // resume: true,
+    roleCreate: true,
+    roleDelete: true,
+    roleUpdate: true,
+    // typingStart: true,
+    // typingStop: true,
+    // userNoteUpdate: true,
+    userUpdate: true,
+    // voiceStateUpdate: true,
+    // warn : true,
+    loggingChannel: -1
+};
+
+module.exports.eventsList = eventsList;
+module.exports.defaultSettings = defaultSettings;
